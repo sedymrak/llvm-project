5b9a0e1b0bbd7579e5f09b538e2b7f40e1d726eb
Matej Košík <matej.kosik@codasip.com>
2026-07-16 11:56:12 +0200

    fix build error
    PROBLEM:
    
    When I configured this project with `./configure.sh` script and then tried to build it with the `./build.sh` script, I've got the following error:
    ```
    [4207/4317] Linking CXX executable bin\lldb-dap.exe
    FAILED: bin/lldb-dap.exe
    C:\WINDOWS\system32\cmd.exe /C "cd . && C:\Users\matej.kosik\msys2\msys2-16.0.0-win11\msys2-x64\mingw64\bin\c++.exe -fvisibility-inlines-hidden -Werror=date-time -Werror=unguarded-availability-new -Wall -Wextra -Wno-unused-parameter -Wwrite-strings -Wcast-qual -Wmissing-field-initializers -pedantic -Wno-long-long -Wc++98-compat-extra-semi -Wimplicit-fallthrough -Wcovered-switch-default -Wno-noexcept-type -Wnon-virtual-dtor -Wdelete-non-virtual-dtor -Wsuggest-override -Wstring-conversion -Wno-pass-failed -Wmisleading-indentation -Wctad-maybe-unsupported -Wno-unknown-pragmas -Wno-strict-aliasing -Wno-vla-extension -g -Xlinker --stack -Xlinker 16777216 -Wl,--delayload=liblldb.dll tools/lldb/tools/lldb-dap/tool/CMakeFiles/lldb-dap.dir/lldb-dap.cpp.obj -o bin\lldb-dap.exe -Wl,--out-implib,lib\liblldb-dap.dll.a -Wl,--major-image-version,0,--minor-image-version,0  lib/libLLVMOption.a  lib/libLLVMSupport.a  lib/liblldbDAP.a  lib/liblldb.dll.a  lib/liblldbHost.a  lib/liblldbUtility.a  -lrpcrt4  lib/libLLVMObject.a  lib/libLLVMIRReader.a  lib/libLLVMBitReader.a  lib/libLLVMAsmParser.a  lib/libLLVMCore.a  lib/libLLVMRemarks.a  lib/libLLVMBitstreamReader.a  lib/libLLVMMCParser.a  lib/libLLVMMC.a  lib/libLLVMDebugInfoDWARFLowLevel.a  lib/libLLVMTextAPI.a  lib/libLLVMBinaryFormat.a  lib/libLLVMTargetParser.a  -lpsapi  -lshell32  -lole32  -luuid  -ladvapi32  -lws2_32  -lntdll  lib/libLLVMDemangle.a  -lkernel32 -luser32 -lgdi32 -lwinspool -lshell32 -lole32 -loleaut32 -luuid -lcomdlg32 -ladvapi32 && cd ."
    ld.lld: error: cannot delay-load liblldb.dll.a(liblldb.dll) due to import of data: __declspec(dllimport) vtable for lldb::SBCommandPluginInterface
    clang-20: error: linker command failed with exit code 1 (use -v to see invocation)
    [4228/4317] Linking CXX executable bin\clang.exe
    ```
    
    SOLUTION
    
    The forward declaration in SBDefines.h marks SBCommandPluginInterface with LLDB_API (i.e., __declspec(dllexport) when building liblldb.dll). Since the class has a virtual destructor, GCC/MinGW exports its vtable as a data symbol from liblldb.dll. LLD's --delayload mechanism can only intercept function imports via thunks — it cannot handle data imports — so the link fails.
    
    The class definition in SBCommandInterpreter.h intentionally has no LLDB_API: it is a user-extendable interface meant to be subclassed outside the DLL. The fix is to remove LLDB_API from its forward declaration in SBDefines.h so that the vtable is not exported from liblldb.dll as a data symbol.

--- lldb/include/lldb/API/SBDefines.h
+++ lldb/include/lldb/API/SBDefines.h
@@ -57,7 +57,7 @@ class LLDB_API SBCommand;
 class LLDB_API SBCommandInterpreter;
 class LLDB_API SBCommandInterpreterRunOptions;
 class LLDB_API SBCommandInterpreterRunResult;
-class LLDB_API SBCommandPluginInterface;
+class SBCommandPluginInterface;
 class LLDB_API SBCommandReturnObject;
 class LLDB_API SBCommunication;
 class LLDB_API SBCompileUnit;
